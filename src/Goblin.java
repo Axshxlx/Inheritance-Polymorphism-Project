7e112498e02736be4345cc63f3e4ff1c810ac7b7
kripa <nakarlekar@gmail.com>
2023-10-19 10:22:24 -0700

    asd

--- src/Goblin.java
+++ src/Goblin.java
@@ -5,19 +5,19 @@ public class Goblin extends Card{
     private static boolean giant = false;
     private static int atkRadius = 4;
     private static int speed;
-    private int xLocation, yLocation;
     private boolean alive;
 
-    public Goblin(boolean alive, int xLocation, int yLocation){
+    public Goblin(boolean alive, int xLocation,     int yLocation){
         super(elixirPrice,strength,speed, atkRadius, giant, alive,xLocation, yLocation);
-        if(getyLocation() > 400) speed=-20;
-        else speed=20;
+        if(getyLocation() > 400) speed=-3;
+        else speed=3;
     }
     public void updateLocation() {
         yLocation+=speed;
     }
-    public void draw(Game game, Goblin test){
-        game.ellipse(test.getxLocation(),test.getyLocation(),25,25);
+    public void draw(Game game){
+        System.out.println("drawing at: " + getyLocation());
+        game.ellipse(getxLocation(),getyLocation(),25,25);
 //        game.p1List.add(this);
     }
 
